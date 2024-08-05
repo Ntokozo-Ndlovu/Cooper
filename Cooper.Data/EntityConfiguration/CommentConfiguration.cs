5b111a2f1b191ecd03ad393c4866f6b6073b0e0f
Ntokozo-Ndlovu <ntokozometo@gmail.com>
2024-08-05 18:12:31 +0200

    Add migration for removing entityId and EntityTable

--- Cooper.Data/EntityConfiguration/CommentConfiguration.cs
+++ Cooper.Data/EntityConfiguration/CommentConfiguration.cs
@@ -18,8 +18,8 @@ namespace Cooper.Data.EntityConfiguration
         private void createSeedData(EntityTypeBuilder<Comment> commentBuilder)
         {
 
-            commentBuilder.HasData(new Comment() { Id = 1 , Body="You are the best"},
-                new Comment() { Id = 2, Body = "You are the best" }
+            commentBuilder.HasData(new Comment() { Id = 1 ,Title ="Comment 1", Body="You are the best"},
+                new Comment() { Id = 2, Title = "Comment 2 ",Body = "You are the best" }
                 );
 
 
